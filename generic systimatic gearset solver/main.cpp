ae6c372829de0adf1738d604e29d15692bc57935
Jarek Ignas-Menzies <jarek@jareks-air.wifi.bcit.ca>
2018-03-27 18:06:53 -0700

    Initial Commit

---
+++ generic systimatic gearset solver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  generic systimatic gearset solver
+//
+//  Created by Jarek Ignas-Menzies on 2018-03-27.
+//  Copyright © 2018 Pimoh Orinx. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
